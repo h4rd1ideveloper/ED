20797e1977f63cbd7361882ae13a0777ca3081e2
ICE <ice@lab05m38.nrc.ice.ufjf.br>
2018-06-18 20:38:45 -0300

    Arvore Binaria

---
+++ ArvBin.h
@@ -0,0 +1,36 @@
+#ifndef ARVBIN_H_INCLUDED
+#define ARVBIN_H_INCLUDED
+#include "NoArv.h"
+
+class ArvBin
+{
+  public:
+    ArvBin();
+    ~ArvBin();
+    int getRaiz();
+    void cria(int x, ArvBin *sae, ArvBin *sad);
+    void anulaRaiz();
+    bool vazia(); // verifica se a árvore está vazia
+    bool busca(int x);
+    void preOrdem();
+    void montaArvore();
+    void insere(int x);
+    int conta();
+    int contaNosFolhas();
+    int altura();
+
+  private:
+    NoArv *raiz; // ponteiro para o No raiz da árvore
+    int auxAltura(NoArv *p);
+    int auxConta(NoArv *p);
+    int auxContaNosFolhas(NoArv *p);
+    NoArv* libera(NoArv *p);
+    bool auxBusca(NoArv *p, int x);
+    void auxPreOrdem(NoArv *p);
+    NoArv* auxMontaArvore();
+    void auxImpNivel(NoArv *p, int atual, int k);
+    void auxInverte(NoArv * p);
+    NoArv* auxInsere(NoArv *p, int x);
+};
+
+#endif // ARVBIN_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "ArvBin.h"
+using namespace std;
+
+int main()
+{
+    ArvBin arv;
+    //arv.montaArvore();
+    arv.insere(25);
+    arv.insere(15);
+    arv.insere(10);
+    arv.preOrdem();
+    cout<< "nos folhas";
+    cout << arv.contaNosFolhas();
+    cout<<"\n Altura\n";
+    cout<<arv.altura();
+    return 0;
+}
